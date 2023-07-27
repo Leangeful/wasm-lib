0e7d93f74878372ae05ebdb44bcf5e274f6e5c22
Leange <cuttheleash1@gmail.com>
2023-07-27 15:22:25 +0200

    hello world

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,17 @@ use wasm_bindgen::prelude::*;
 #[wasm_bindgen]
 extern "C" {
     fn alert(s: &str);
+
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+macro_rules! console_log {
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
 }
 
 #[wasm_bindgen]
 pub fn greet() {
     alert("Hello, wasm-lib!");
+    console_log!("Hello, console!");
 }
